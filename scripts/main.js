d4191268756b0a53f25d1d7072823ad1fa517f42
Axel <axelviskovic@gmail.com>
2019-05-07 23:06:18 +0200

    fleche
    a

--- scripts/main.js
+++ scripts/main.js
@@ -4,15 +4,18 @@ const point3 = document.querySelector("#point3")
 const sliderContainer = document.querySelector(".sliderContainer")
 const contactOnglet = document.querySelector("#contactOnglet")
 const onglet = document.querySelector("#contact")
-
+const arrowRight = document.querySelector("#arrowRight")
+const arrowLeft = document.querySelector("#arrowLeft")
 
 // SLIDER
+
+let nbImage = 0
 
 point1.addEventListener(
   "click",
   function(){
       sliderContainer.style.transform="translateX(0%)"
-      sliderContainer.style.transform="translateX(0%)"
+      nbImage=0
   }
 )
 
@@ -20,7 +23,7 @@ point2.addEventListener(
   "click",
   function(){
       sliderContainer.style.transform="translateX(-100%)"
-      sliderContainer.style.transform="translateX(-100%)"
+      nbImage=1
   }
 )
 
@@ -28,6 +31,38 @@ point3.addEventListener(
   "click",
   function(){
       sliderContainer.style.transform="translateX(-200%)"
+      nbImage=2
+  }
+)
+
+arrowRight.addEventListener(
+  "click",
+  function(){
+    if(nbImage==2){
+      nbImage=0
+      sliderContainer.style.transform="translateX(0%)"
+      console.log(nbImage)
+    }
+    else{
+      nbImage++
+      sliderContainer.style.transform="translateX("+nbImage*-100+"%)"
+      console.log(nbImage)
+    }
+  }
+)
+
+arrowLeft.addEventListener(
+  "click",
+  function(){
+    if(nbImage==0){
+      nbImage=2
       sliderContainer.style.transform="translateX(-200%)"
+      console.log(nbImage)
+    }
+    else{
+      nbImage=nbImage-1
+      sliderContainer.style.transform="translateX("+nbImage*-100+"%)"
+      console.log(nbImage)
+    }
   }
 )
