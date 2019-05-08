d69fb43c1f8e670062b5dad0fd8a2268274f8f0a
Axel <axelviskovic@gmail.com>
2019-05-08 13:54:55 +0200

    a
    a

--- scripts/main.js
+++ scripts/main.js
@@ -6,16 +6,44 @@ const contactOnglet = document.querySelector("#contactOnglet")
 const onglet = document.querySelector("#contact")
 const arrowRight = document.querySelector("#arrowRight")
 const arrowLeft = document.querySelector("#arrowLeft")
+let   nbImage = 0
+let action
+setCurrent()
 
 // SLIDER
 
-let nbImage = 0
+
+function setCurrent(){
+  if(nbImage==0){
+    point1.classList.add("current")
+    point2.classList.remove("current")
+    point3.classList.remove("current")
+  }
+
+  if(nbImage==1){
+    point2.classList.add("current")
+    point1.classList.remove("current")
+    point3.classList.remove("current")
+  }
+
+  if(nbImage==2){
+    point3.classList.add("current")
+    point2.classList.remove("current")
+    point1.classList.remove("current")
+  }
+}
+
+
 
 point1.addEventListener(
   "click",
   function(){
       sliderContainer.style.transform="translateX(0%)"
       nbImage=0
+      setCurrent(action)
+      /*point1.classList.add("current")
+      point2.classList.remove("current")
+      point3.classList.remove("current")*/
   }
 )
 
@@ -24,6 +52,10 @@ point2.addEventListener(
   function(){
       sliderContainer.style.transform="translateX(-100%)"
       nbImage=1
+      setCurrent(action)
+      /*point2.classList.add("current")
+      point1.classList.remove("current")
+      point3.classList.remove("current")*/
   }
 )
 
@@ -32,6 +64,10 @@ point3.addEventListener(
   function(){
       sliderContainer.style.transform="translateX(-200%)"
       nbImage=2
+      setCurrent(action)
+      /*point3.classList.add("current")
+      point1.classList.remove("current")
+      point2.classList.remove("current")*/
   }
 )
 
@@ -41,12 +77,12 @@ arrowRight.addEventListener(
     if(nbImage==2){
       nbImage=0
       sliderContainer.style.transform="translateX(0%)"
-      console.log(nbImage)
+      setCurrent(action)
     }
     else{
       nbImage++
       sliderContainer.style.transform="translateX("+nbImage*-100+"%)"
-      console.log(nbImage)
+      setCurrent(action)
     }
   }
 )
@@ -57,12 +93,12 @@ arrowLeft.addEventListener(
     if(nbImage==0){
       nbImage=2
       sliderContainer.style.transform="translateX(-200%)"
-      console.log(nbImage)
+      setCurrent(action)
     }
     else{
       nbImage=nbImage-1
       sliderContainer.style.transform="translateX("+nbImage*-100+"%)"
-      console.log(nbImage)
+      setCurrent(action)
     }
   }
 )
